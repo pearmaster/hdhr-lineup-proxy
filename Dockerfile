7f18f39144fce3e27bda6c5ddce4675f13ac3492
Jacob Brunson <github@jacobbrunson.com>
2022-07-03 23:20:44 +0000

    build up a new file

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,14 @@ RUN apk add --no-cache jq
 RUN apk add --no-cache make
 RUN apk add --no-cache gcc
 RUN apk add --no-cache musl-dev
+RUN apk add --no-cache python3
 
 COPY ./download-data.sh /docker-entrypoint.d/30-download-data.sh
 COPY ./proxy.conf /etc/nginx/conf.d/proxy.conf
+COPY ./convert_lineup.py /usr/local/share/convert_lineup.py
 
 VOLUME /data
 
 ADD http://download.silicondust.com/hdhomerun/libhdhomerun_20210624.tgz /tmp
 RUN cd /tmp && tar zxvf libhdhomerun_20210624.tgz && cd libhdhomerun && make && cp hdhomerun_config /usr/local/bin
+
--- download-data.sh
+++ download-data.sh
@@ -2,18 +2,34 @@
 
 LINEUP_FILE="/data/lineup.json"
 
+function make_file() {
+    TUNER=$1
+    DEVICE_ID=$2
+    hdhomerun_config scan ${HDHR_IP_ADDR} ${TUNER} /tmp/scanresults.txt
+    /usr/bin/python3 /usr/local/share/convert_lineup.py ${DEVICE_ID} /tmp/scanresults.txt ${LINEUP_FILE}
+    return $?
+}
+
 if [ -n "${HDHR_IP_ADDR}" ]; then
     if [ ! -f "${LINEUP_FILE}"  ]; then
-        LINEUP_URL=$(wget -q http://${HDHR_IP_ADDR}/discover.json -O - | jq -r '.LineupURL')
-        if [ -n "$LINEUP_URL" ]; then
-            wget -q ${LINEUP_URL} -O ${LINEUP_FILE}
-            WGET_RC=$?
-            if [ "$WGET_RC" -ne 0 ]; then
-                echo "wget failure to get line up" >&2
+        DEVICE_ID=$(wget -q http://${HDHR_IP_ADDR}/discover.json -O - | jq -r '.DeviceId')
+        if [ -n "$DEVICE_ID" ]; then
+            hdhomerun_config ${HDHR_IP_ADDR} get /tuner0/status | grep 'ch=none' > /dev/null
+            if [ $? -eq 0 ]; then
+                make_file 0 $DEVICE_ID
+                exit $?
+            else
+                hdhomerun_config ${HDHR_IP_ADDR} get /tuner1/status | grep 'ch=none' > /dev/null
+                if [ $? -eq 0 ]; then
+                    make_file 1 $DEVICE_ID
+                    exit $?
+                else
+                    echo "Tuner not available" >&2
+                    exit 1
+                fi
             fi
-            return $WGET_RC
         else
-            echo "Could not find Lineup URL" >&2
+            echo "Could not find DeviceId" >&2
         fi
     else
         echo "Lineup file already exists" >&2
