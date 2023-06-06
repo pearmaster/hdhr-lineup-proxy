12eae8bc9058e87acc188bc1246e4593b40ef2e6
Jacob Brunson <github@jacobbrunson.com>
2023-06-05 18:56:51 -0600

    always create file

--- download-data.sh
+++ download-data.sh
@@ -11,30 +11,27 @@ function make_file() {
 }
 
 if [ -n "${HDHR_IP_ADDR}" ]; then
-    if [ ! -f "${LINEUP_FILE}"  ]; then
-        DEVICE_ID=$(wget -q http://${HDHR_IP_ADDR}/discover.json -O - | jq -r '.DeviceId')
-        if [ -n "$DEVICE_ID" ]; then
-            hdhomerun_config ${HDHR_IP_ADDR} get /tuner0/status | grep 'ch=none' > /dev/null
+    DEVICE_ID=$(wget -q http://${HDHR_IP_ADDR}/discover.json -O - | jq -r '.DeviceId')
+    echo "Device ID is ${DEVICE_ID}"
+    if [ -n "$DEVICE_ID" ]; then
+        hdhomerun_config ${HDHR_IP_ADDR} get /tuner0/status | grep 'ch=none' > /dev/null
+        if [ $? -eq 0 ]; then
+            make_file 0 $DEVICE_ID
+            exit $?
+        else
+            hdhomerun_config ${HDHR_IP_ADDR} get /tuner1/status | grep 'ch=none' > /dev/null
             if [ $? -eq 0 ]; then
-                make_file 0 $DEVICE_ID
+                make_file 1 $DEVICE_ID
                 exit $?
             else
-                hdhomerun_config ${HDHR_IP_ADDR} get /tuner1/status | grep 'ch=none' > /dev/null
-                if [ $? -eq 0 ]; then
-                    make_file 1 $DEVICE_ID
-                    exit $?
-                else
-                    echo "Tuner not available" >&2
-                    exit 1
-                fi
+                echo "Tuner not available" >&2
+                exit 1
             fi
-        else
-            echo "Could not find DeviceId" >&2
         fi
     else
-        echo "Lineup file already exists" >&2
-        exit 0
+        echo "Could not find DeviceId" >&2
     fi
+
 else
     echo "HDHR IP Address not provided" >&2
 fi
