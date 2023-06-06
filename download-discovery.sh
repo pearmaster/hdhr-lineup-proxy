5307840aa340da38e702c6f9ff9492417c85e946
Jacob Brunson <github@jacobbrunson.com>
2023-06-05 21:37:54 -0600

    Update download-discovery.sh

--- download-discovery.sh
+++ download-discovery.sh
@@ -3,10 +3,12 @@
 DISCOVER_FILE=/data/discover.json
 export MY_IP=$(ip route get 8.8.8.8 | awk 'NR==1 {print $NF}')
 
-wget http://${HDHR_IP_ADDR}/discover.json -O $DISCOVER_FILE
+wget http://${HDHR_IP_ADDR}/discover.json -O ${DISCOVER_FILE}.new
 
-sed -i 's/https:/http:/g' $DISCOVER_FILE
+sed -i 's/https:/http:/g' ${DISCOVER_FILE}.new
 
-sed -i "s/api.hdhomerun.com/${MY_IP}/g" $DISCOVER_FILE
+sed -i "s/api.hdhomerun.com/${MY_IP}/g" ${DISCOVER_FILE}.new
+
+mv ${DISCOVER_FILE}.new ${DISCOVER_FILE}
 
 exit 0
